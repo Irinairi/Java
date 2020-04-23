297cb83bc35278a14d91d3d36b4b9d2ee5d2774e
IRINA <irinabrinza@yahoo.ro>
2020-04-23 23:37:04 +0300

    LAB 9

---
+++ src/com/company/Main.java
@@ -0,0 +1,80 @@
+package com.company;
+
+import javax.print.DocFlavor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+System.out.println("Hello");
+System.out.println("Irina");
+  int myInt = 1450 ;
+  byte myByte = 123;
+               int resultat = myInt + myByte ;
+          System.out.println(resultat);
+
+          short myShort = 111 ;
+          long myLong = 2225;
+         long  resultat2 = myLong / myShort ;
+        System.out.println( resultat2 );
+
+
+           char myChar =6 ;
+           double myDouble = 8;
+
+           double resultat3 = myChar * myDouble ;
+           System.out.println(resultat3);
+
+
+        float myfloat = -5 ;
+        System.out.println(resultat3 + myfloat ) ;
+
+        byte h = 55 ;
+        short f = 9;
+                   float resultat4 = h + f ;
+                   System.out.println( resultat4);
+
+           int g = 9 ;
+          float resultat5= resultat4 % 9 ;
+                   System.out.println(resultat5) ;
+
+          float c = -3;
+          short v = 5 ;
+          float resultat6 = c * v ;
+                     System.out.println(resultat6);
+          int b =8;
+          float resultat7 = (resultat6 / b);
+                      System.out.println(resultat7 ) ;
+
+          byte s = 20 ;
+                  float resultat8 =resultat7 + s ;
+
+                     System.out.println(resultat8 );
+
+                  byte m =15;
+                  short y = 3;
+                  float resulatat9  = (m/y) ;
+                       System.out.println(resulatat9);
+             short x = 2;
+             float resultatz =resulatat9 * x ;
+                      System.out.println(resultatz);
+             short w =8;
+             float z = 3;
+             float resultata = w % z ;
+                              System.out.println(resultata);
+                              float resultat11 =(resultatz-resultata);
+
+                              System.out.println(resultat11);
+
+                   int q = 5 ;
+                   float resultate = (resultat11 + q ) ;
+                   System.out.println(resultate);
+
+
+
+
+
+
+    }
+}
